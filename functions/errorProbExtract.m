8b439007b8fc7b13e48e41ce71b63d8998a7de80
oliver-brandon <105843243+oliver-brandon@users.noreply.github.com>
2023-08-06 09:38:11 -0700

    Update errorProbExtract.m
    added ability to extract all cues preceding error types for both levers

--- functions/errorProbExtract.m
+++ functions/errorProbExtract.m
@@ -1,13 +1,31 @@
-function errorProbTS = errorProbExtract( ...
-    trial_type, ...
-    session_ts, ...
+function [data, errorProbLeverTS, errorProbCueTS] = errorProbExtract(...
+    data,...
+    session_identifiers, ...
     errorType, ...
-    lever...
+    lever ...
     )
-% errorType: 1 = winStay, 2 = winShift, 3 = loseStay, 4 = loseShift
-% lever: errors based on correct lever = 1, incorrect lever = 2
+% This function will extract lever and cue timestamps for win-stay, win-shift, lose-stay,
+% and lose-shift error probabilities. It will output two arrays:
+% errorProbLeverTS and errorProbCueTS. Column 1 is the timestamps
+% associated with the win/lose trial and column 2 is the timestamps
+% associated with the stay/shift trial.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Args:
+%% session_identifiers: 
+% An Nx2 array with cue and lever time stamps in order
+% of occurance in column 1 and a number associated with each timestamp in
+% column 2 that represents the TTL (0 = cue, 1 = cRew, 2 = cNoRew, 3 =
+% iRew, 4 = iNoRew). Can be extracted using the function sessionArraySort.m
+%% errorType: 
+% 1 = winStay, 2 = winShift, 3 = loseStay, 4 = loseShift
+%% lever: 
+% errors based on correct lever = 1, incorrect lever = 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize index array
-index_array = [];
+win_index_array = [];
+lose_index_array = [];
+stay_index_array = [];
+shift_index_array = [];
 if lever == 1
     reward = 1;
     noreward = 2;
@@ -21,42 +39,115 @@ elseif lever == 2
 end
 if errorType == 1
     % Loop through rows of trial_type
-    for i = 2:size(session_ts,1)
-        if (trial_type(i,:) == reward || trial_type(i,:) == noreward) && trial_type(i-1,:) == reward
+    for i = 4:2:size(session_identifiers,1)
+        if (session_identifiers(i,2) == reward || session_identifiers(i,2) == noreward) && session_identifiers(i-2,2) == reward
             
-            index_array(end+1) = i; % add current index to index array
+            win_index_array(end+1,1) = i - 2;
+            stay_index_array(end+1,1) = i; % add current index to index array
         end
     end
+    % Extract win-stay timestamps
+    errorProbLeverTS(:,1) = session_identifiers(win_index_array,1);
+    errorProbLeverTS(:,2) = session_identifiers(stay_index_array,1);
+    errorProbCueTS(:,1) = session_identifiers(win_index_array-1,1);
+    errorProbCueTS(:,2) = session_identifiers(stay_index_array-1,1);
 elseif errorType == 2
     % Loop through rows of trial_type
-    for i = 2:size(session_ts,1)
-        if (trial_type(i,:) == shift1 || trial_type(i,:) == shift2) && trial_type(i-1,:) == reward
+    for i = 4:2:size(session_identifiers,1)
+        if (session_identifiers(i,2) == shift1 || session_identifiers(i,2) == shift2) && session_identifiers(i-2,2) == reward
             
-            index_array(end+1) = i; % add current index to index array
+            win_index_array(end+1,1) = i - 2;
+            shift_index_array(end+1,1) = i; % add current index to index array
         end
     end
+    % Extract win-shift timestamps
+    errorProbLeverTS(:,1) = session_identifiers(win_index_array,1);
+    errorProbLeverTS(:,2) = session_identifiers(shift_index_array,1);
+    errorProbCueTS(:,1) = session_identifiers(win_index_array-1,1);
+    errorProbCueTS(:,2) = session_identifiers(shift_index_array-1,1);
 elseif errorType == 3
     % Loop through rows of trial_type
-    for i = 2:size(session_ts,1)
-        if (trial_type(i,:) == reward || trial_type(i,:) == noreward) && trial_type(i-1,:) == noreward
+    for i = 4:2:size(session_identifiers,1)
+        if (session_identifiers(i,2) == reward || session_identifiers(i,2) == noreward) && session_identifiers(i-2,2) == noreward
             
-            index_array(end+1) = i; % add current index to index array
+            lose_index_array(end+1,1) = i - 2;
+            stay_index_array(end+1,1) = i; % add current index to index array
         end
     end
+    % Extract lose-stay timestamps
+    errorProbLeverTS(:,1) = session_identifiers(lose_index_array,1);
+    errorProbLeverTS(:,2) = session_identifiers(stay_index_array,1);
+    errorProbCueTS(:,1) = session_identifiers(lose_index_array-1,1);
+    errorProbCueTS(:,2) = session_identifiers(stay_index_array-1,1);
 elseif errorType == 4
-% Loop through rows of trial_type
-for i = 2:size(session_ts,1)
-    if (trial_type(i,:) == shift1 || trial_type(i,:) == shift2) && trial_type(i-1,:) == noreward
-        
-        index_array(end+1) = i; % add current index to index array
+    % Loop through rows of trial_type
+    for i = 4:2:size(session_identifiers,1)
+        if (session_identifiers(i,2) == shift1 || session_identifiers(i,2) == shift2) && session_identifiers(i-2,2) == noreward
+            
+            lose_index_array(end+1,1) = i - 2;
+            shift_index_array(end+1,1) = i; % add current index to index array
+        end
     end
-end
+    % Extract lose-shift timestamps
+    errorProbLeverTS(:,1) = session_identifiers(lose_index_array,1);
+    errorProbLeverTS(:,2) = session_identifiers(shift_index_array,1);
+    errorProbCueTS(:,1) = session_identifiers(lose_index_array-1,1);
+    errorProbCueTS(:,2) = session_identifiers(shift_index_array-1,1);
 else
     disp('Select a valid error probability to grab')
 end
-if isempty(index_array)
-    errorProbTS = [];
-else
-    % Extract values from input2 using index_array
-    errorProbTS = session_ts(index_array,:);
+
+% Creates new TTLs
+if errorType == 1
+    % lever TTLs
+    data.epocs.WIN_stay_LEV.name = 'WIN_stay_LEV';
+    data.epocs.WIN_stay_LEV.onset = errorProbLeverTS(:,1);
+    data.epocs.WIN_stay_LEV.offset = errorProbLeverTS(:,1) + 1;
+    data.epocs.WIN_stay_LEV.data = ones(height(errorProbLeverTS(:,1)))*5;
+    data.epocs.win_STAY_LEV.name = 'win_STAY_LEV';
+    data.epocs.win_STAY_LEV.onset = errorProbLeverTS(:,2);
+    data.epocs.win_STAY_LEV.offset = errorProbLeverTS(:,2) + 1;
+    data.epocs.win_STAY_LEV.data = ones(height(errorProbLeverTS(:,2)))*6;
+    % cue TTLs
+    data.epocs.WIN_stay_CUE.name = 'WIN_stay_CUE';
+    data.epocs.WIN_stay_CUE.onset = errorProbCueTS(:,1);
+    data.epocs.WIN_stay_CUE.offset = errorProbCueTS(:,1) + 1;
+    data.epocs.WIN_stay_CUE.data = ones(height(errorProbCueTS(:,1)))*7;
+    data.epocs.win_STAY_CUE.name = 'win_STAY_CUE';
+    data.epocs.win_STAY_CUE.onset = errorProbCueTS(:,2);
+    data.epocs.win_STAY_CUE.offset = errorProbCueTS(:,2) + 1;
+    data.epocs.win_STAY_CUE.data = ones(height(errorProbCueTS(:,2)))*8;
+elseif errorType == 2
+    % lever TTLs
+    data.epocs.WIN_shift_LEV.name = 'WIN_shift_LEV';
+    data.epocs.WIN_shift_LEV.onset = errorProbLeverTS(:,1);
+    data.epocs.win_SHIFT_LEV.name = 'win_SHIFT_LEV';
+    data.epocs.win_SHIFT_LEV.onset = errorProbLeverTS(:,2);
+    % cue TTLs
+    data.epocs.WIN_shift_CUE.name = 'WIN_shift_CUE';
+    data.epocs.WIN_shift_CUE.onset = errorProbCueTS(:,1);
+    data.epocs.win_SHIFT_CUE.name = 'win_SHIFT_CUE';
+    data.epocs.win_SHIFT_CUE.onset = errorProbCueTS(:,2);
+elseif errorType == 3
+    % lever TTLs
+    data.epocs.LOS_stay_LEV.name = 'LOS_stay_LEV';
+    data.epocs.LOS_stay_LEV.onset = errorProbLeverTS(:,1);
+    data.epocs.los_STAY_LEV.name = 'los_STAY_LEV';
+    data.epocs.los_STAY_LEV.onset = errorProbLeverTS(:,2);
+    % cue TTLs
+    data.epocs.LOS_stay_CUE.name = 'LOS_stay_CUE';
+    data.epocs.LOS_stay_CUE.onset = errorProbCueTS(:,1);
+    data.epocs.los_STAY_CUE.name = 'los_STAY_CUE';
+    data.epocs.los_STAY_CUE.onset = errorProbCueTS(:,2);
+elseif errorType == 4
+    % lever TTLs
+    data.epocs.LOS_shift_LEV.name = 'LOS_shift_LEV';
+    data.epocs.LOS_shift_LEV.onset = errorProbLeverTS(:,1);
+    data.epocs.los_SHIFT_LEV.name = 'los_SHIFT_LEV';
+    data.epocs.los_SHIFT_LEV.onset = errorProbLeverTS(:,2);
+    % cue TTLs
+    data.epocs.LOS_shift_CUE.name = 'LOS_shift_CUE';
+    data.epocs.LOS_shift_CUE.onset = errorProbCueTS(:,1);
+    data.epocs.los_SHIFT_CUE.name = 'los_SHIFT_CUE';
+    data.epocs.los_SHIFT_CUE.onset = errorProbCueTS(:,2);
 end
