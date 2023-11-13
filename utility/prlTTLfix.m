1ac31a021926b4dcf6740cce6bbc21ca591341ff
oliver-brandon <105843243+oliver-brandon@users.noreply.github.com>
2023-11-13 17:48:22 -0500

    utility to fix missing TTLs from prl experiment

---
+++ utility/prlTTLfix.m
@@ -0,0 +1,63 @@
+BLOCKPATH = '/Volumes/CUDADRIVE/PRL_TTL/PRL_TTL/NE4M_Rev1_NA_Empty_NA_NA';
+tank = TDTbin2mat(BLOCKPATH,'TYPE',{'streams','epocs'});
+
+% matpath = '/Users/brandon/My Drive/prl/PRL_GRABDA/Extract TTLs/343M_Rev1_VEH.mat';
+% load(matpath)
+
+
+
+% data.epocs.St1_.name = 'St1_';
+% data.epocs.St1_.onset = [];
+% data.epocs.St1_.offset = [];
+% data.epocs.St1_.data = zeros(30,1);
+% data.epocs.cRewA.name = 'cRewA';
+% data.epocs.cRewA.onset = [];
+% data.epocs.cRewA.offset = [];
+% data.epocs.cRewA.data = ones(22,1);
+% data.epocs.cNoRewA.name = 'cNoRewA';
+% data.epocs.cNoRewA.onset = [];
+% data.epocs.cNoRewA.offset = [];
+% data.epocs.cNoRewA.data = ones(2,1)*2;
+% data.epocs.iRewA.name = 'iRewA';
+% data.epocs.iRewA.onset = [];
+% data.epocs.iRewA.offset = [];
+% data.epocs.iRewA.data = [];
+% data.epocs.iNoRewA.name = 'iNoRewA';
+% data.epocs.iNoRewA.onset = [];
+% data.epocs.iNoRewA.offset = [];
+% data.epocs.iNoRewA.data = ones(6,1)*4;
+% 
+% data.epocs.St1_.offset = data.epocs.St1_.onset+1
+% data.epocs.cRewA.offset = data.epocs.cRewA.onset+1
+% data.epocs.cNoRewA.offset = data.epocs.cNoRewA.onset+1
+% data.epocs.iRewA.offset = data.epocs.iRewA.onset+1
+% data.epocs.iNoRewA.offset = data.epocs.iNoRewA.onset+1
+
+% data.epocs.St2_.name = 'St2_';
+% data.epocs.St2_.onset = [];
+% data.epocs.St2_.offset = [];
+% data.epocs.St2_.data = zeros(30,1);
+% data.epocs.cRewC.name = 'cRewC';
+% data.epocs.cRewC.onset = [];
+% data.epocs.cRewC.offset = [];
+% data.epocs.cRewC.data = ones(27,1);
+% data.epocs.cNoRewC.name = 'cNoRewC';
+% data.epocs.cNoRewC.onset = [];
+% data.epocs.cNoRewC.offset = [];
+% data.epocs.cNoRewC.data = ones(2,1)*2;
+% data.epocs.iRewC.name = 'iRewC';
+% data.epocs.iRewC.onset = [];
+% data.epocs.iRewC.offset = [];
+% data.epocs.iRewC.data = [];
+% data.epocs.iNoRewC.name = 'iNoRewC';
+% data.epocs.iNoRewC.onset = [];
+% data.epocs.iNoRewC.offset = [];
+% data.epocs.iNoRewC.data = ones(1,1)*4;
+% 
+% data.epocs.St2_.offset = data.epocs.St2_.onset+1
+% data.epocs.cRewC.offset = data.epocs.cRewC.onset+1
+% data.epocs.cNoRewC.offset = data.epocs.cNoRewC.onset+1
+% data.epocs.iRewC.offset = data.epocs.iRewC.onset+1
+% data.epocs.iNoRewC.offset = data.epocs.iNoRewC.onset+1
+
+
